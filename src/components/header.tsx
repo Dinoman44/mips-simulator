76de3f5d9f562b8b6bba7d1c36fc22402b5453ad
Dinoman44 <prakamya.singh08@gmail.com>
2025-12-18 17:54:16 +0800

    Add new home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,13 +7,13 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-            <Navbar.Brand href="#"><img src="/src/assets/logo.png" alt="MIPS Simulator Logo" style={{ height: '30px' }} /></Navbar.Brand>
-            <Navbar.Brand href="#">MIPS Simulator</Navbar.Brand>
+            <Navbar.Brand href="/"><img src="/src/assets/logo.png" alt="MIPS Simulator Logo" style={{ height: '30px' }} /></Navbar.Brand>
+            <Navbar.Brand href="/">MIPS Simulator</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="/index.html">Encoder-Decoder</Nav.Link>
-                <Nav.Link href="/mips.html">MIPS Instructions</Nav.Link>
+                <Nav.Link href="/pages/decode-encode.html">Encoder-Decoder</Nav.Link>
+                <Nav.Link href="/pages/mips.html">MIPS Instructions</Nav.Link>
                 <Nav.Link href="#">Simulator</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
