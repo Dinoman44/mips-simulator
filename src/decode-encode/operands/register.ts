f3e196fee48154dbf1903e326fcd9a497332cb6c
Dinoman44 <prakamya.singh08@gmail.com>
2025-12-18 17:43:54 +0800

    Fix error message for reserved registers

--- src/decode-encode/operands/register.ts
+++ src/decode-encode/operands/register.ts
@@ -22,7 +22,7 @@ class Register {
     private readonly _label: string;
     private readonly _number: number;
     private readonly _binaryString: string;
-    static readonly register_mapping: TwoWayMapRegisterToNumber = new TwoWayMapRegisterToNumber([
+    static readonly registerMapping: TwoWayMapRegisterToNumber = new TwoWayMapRegisterToNumber([
         ["$zero", 0],
         ["$v0", 2],
         ["$v1", 3],
@@ -49,37 +49,40 @@ class Register {
         ["$t8", 24],
         ["$t9", 25],
     ]);
-    static readonly reservedRegisters: Set<number> = new Set<number>([1, 26, 27, 28, 29, 30, 31]);
+    static readonly reservedRegisterNumbers: Set<number> = new Set<number>([1, 26, 27, 28, 29, 30, 31]);
+    static readonly reservedRegisterLabels: Set<string> = new Set<string>(["$at", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"]);
 
     static parseRegisterForNumber(reg: string): Register {
         if (/^\$\d+$/gi.test(reg)) {
             let regNum: number = parseInt(reg.slice(1), 10);
-            console.log(regNum);
-            if (this.register_mapping.isValidNumber(regNum)) {
-                return new Register(this.register_mapping.getRegister(regNum)!, regNum);
+            if (this.registerMapping.isValidNumber(regNum)) {
+                return new Register(this.registerMapping.getRegister(regNum)!, regNum);
             } else if (regNum < 0 || regNum > 31) {
                 throw new Error(`Register number out of range (0-31): ${reg}`);
-            } else if (this.reservedRegisters.has(regNum)) {
+            } else if (this.reservedRegisterNumbers.has(regNum)) {
                 throw new Error(`Register number ${regNum} is reserved and cannot be used.`);
-            } else {
-                throw new Error(`${regNum} is not a valid register.`);
             }
+            throw new Error(`$${regNum} is not a valid register.`);
         } else if (/^\$(zero|[vats]\d+)$/gi.test(reg)) {
-            if (this.register_mapping.isValidRegister(reg)) {
-                return new Register(reg, this.register_mapping.getNumber(reg)!);
+            if (this.registerMapping.isValidRegister(reg)) {
+                return new Register(reg, this.registerMapping.getNumber(reg)!);
+            } else if (this.reservedRegisterLabels.has(reg)) {
+                throw new Error(`Register "${reg}" is reserved and cannot be used.`);
             }
             throw new Error(`"${reg}" is not a valid register.`);
+        } else if (this.reservedRegisterLabels.has(reg)) {
+            throw new Error(`Register "${reg}" is reserved and cannot be used.`);
         } else {
             throw new Error(`Invalid register format: ${reg}`);
         }
     }
 
     static parseRegisterForLabel(regNum: number): Register {
-        if (this.register_mapping.isValidNumber(regNum)) {
-            return new Register(this.register_mapping.getRegister(regNum)!, regNum);
+        if (this.registerMapping.isValidNumber(regNum)) {
+            return new Register(this.registerMapping.getRegister(regNum)!, regNum);
         } else if (regNum < 0 || regNum > 31) {
             throw new Error(`Register number out of range (0-31): ${regNum}`);
-        } else if (regNum in this.reservedRegisters) {
+        } else if (this.reservedRegisterNumbers.has(regNum)) {
             throw new Error(`Register number ${regNum} is reserved and cannot be used.`);
         } else {
             throw new Error(`${regNum} is not a valid register.`);
